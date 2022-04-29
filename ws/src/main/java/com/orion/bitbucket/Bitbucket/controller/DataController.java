47d554cf971cb7312ba68578e967fa17e7074d4f
Oguzhan Ulusoy <oguzhan.ulusoy@mera.com>
2022-04-29 18:12:24 +0300

    implemented a new controller that handles getting data mechanism

--- ws/src/main/java/com/orion/bitbucket/Bitbucket/controller/DataController.java
+++ ws/src/main/java/com/orion/bitbucket/Bitbucket/controller/DataController.java
@@ -1,2 +1,22 @@
-package com.orion.bitbucket.Bitbucket.controller;public class DataController {
+package com.orion.bitbucket.Bitbucket.controller;
+
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.orion.bitbucket.Bitbucket.service.BaseServiceIF;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class DataController {
+
+    @Autowired
+    private BaseServiceIF baseService;
+
+    @RequestMapping(value = "/setup", method = RequestMethod.GET)
+    public String getData() {
+        baseService.getData();
+        return "index.html";
+    }
 }
