c8d6177b62a966c08599b288f5f9659fefcd71f9
yusufakbs <yusuf.akbas@mera.com>
2022-04-29 10:23:12 +0300

    arrangements were made

--- ws/src/main/java/com/orion/bitbucket/Bitbucket/controller/PageController.java
+++ ws/src/main/java/com/orion/bitbucket/Bitbucket/controller/PageController.java
@@ -7,18 +7,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import com.orion.bitbucket.Bitbucket.model.AuthorTotalPRs;
+import com.orion.bitbucket.Bitbucket.model.AuthorDO;
+import com.orion.bitbucket.Bitbucket.service.AuthorServiceIF;
+import com.orion.bitbucket.Bitbucket.service.BaseServiceIF;
 import com.orion.bitbucket.Bitbucket.service.PullRequestService;
+import com.orion.bitbucket.Bitbucket.service.PullRequestServiceIF;
 
 @Controller
 public class PageController {
 
    @Autowired
-   private PullRequestService service;
+   private BaseServiceIF baseService;
+   private PullRequestServiceIF pullRequestService;
+   private AuthorServiceIF authorServiceIF;
    
    @RequestMapping(value = "/", method = RequestMethod.GET)
    public String edit(Model model) throws  UnirestException {
-       service.getData(); // Bu metot uygulamanin baslangicinda cagirilmali ki listeler doldurulsun. Hangi kisim ilk calisacaksa bu orada cagirilmali.
+       baseService.getData(); // Bu metot uygulamanin baslangicinda cagirilmali ki listeler doldurulsun. Hangi kisim ilk calisacaksa bu orada cagirilmali.
       //List<PullRequest> pullrequests = service.reportAll();
       //model.addAttribute("pr", new PullRequest());
       //model.addAttribute("pullrequests", pullrequests);
@@ -27,8 +32,8 @@ public class PageController {
 
    @RequestMapping(value = "/test", method = RequestMethod.GET)
    public String test(Model model) throws  UnirestException {
-      List<AuthorTotalPRs> getAllAuthor = service.getCountOfPrStatesOfAllAuthor();
-      model.addAttribute("tst", new AuthorTotalPRs());
+      List<AuthorDO> getAllAuthor = authorServiceIF.getCountOfPrStatesOfAllAuthor();
+      model.addAttribute("tst", new AuthorDO());
       model.addAttribute("getAllAuthor", getAllAuthor);
       return "index.html";
    }
