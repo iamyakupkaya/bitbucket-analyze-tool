e1d46f7d8b061ea68c207f645fb17d876f1c618a
Oguzhan Ulusoy <oguzhan.ulusoy@mera.com>
2022-04-29 18:44:42 +0300

    renamed a class for author

--- ws/src/main/java/com/orion/bitbucket/Bitbucket/controller/AuthorController.java
+++ ws/src/main/java/com/orion/bitbucket/Bitbucket/controller/AuthorController.java
@@ -1,7 +1,6 @@
 package com.orion.bitbucket.Bitbucket.controller;
 
-import com.mashape.unirest.http.exceptions.UnirestException;
-import com.orion.bitbucket.Bitbucket.service.BaseServiceIF;
+import com.orion.bitbucket.Bitbucket.service.AuthorServiceIF;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,14 +8,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class DataController {
+public class AuthorController {
 
     @Autowired
-    private BaseServiceIF baseService;
+    private AuthorServiceIF authorServiceIF;
 
-    @RequestMapping(value = "/setup", method = RequestMethod.GET)
-    public String getData() {
-        baseService.getData();
+    @RequestMapping(value = "/author-page", method = RequestMethod.GET)
+    public String authorMain(Model model) {
+        int count = authorServiceIF.getAuthorCount();
         return "index.html";
     }
 }
