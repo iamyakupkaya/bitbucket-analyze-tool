79d8166ec4649828118b4f54cf69df383e56c761
Oguzhan Ulusoy <oguzhan.ulusoy@mera.com>
2022-04-29 18:42:21 +0300

    reverted some changes

--- ws/src/main/java/com/orion/bitbucket/Bitbucket/service/BaseService.java
+++ ws/src/main/java/com/orion/bitbucket/Bitbucket/service/BaseService.java
@@ -1,19 +1,22 @@
 package com.orion.bitbucket.Bitbucket.service;
-import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.time.Instant;
-import java.util.*;
+
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import com.orion.bitbucket.Bitbucket.model.BranchDO;
 import com.orion.bitbucket.Bitbucket.model.PullRequestDO;
 import com.orion.bitbucket.Bitbucket.model.ReviewerDO;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import com.mashape.unirest.http.exceptions.UnirestException;
+
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+
+import com.orion.bitbucket.Bitbucket.service.utils.DataParser;
 
 @Service
 public class BaseService implements BaseServiceIF {
@@ -63,36 +66,36 @@ public class BaseService implements BaseServiceIF {
 		return list;
 	}
 
-	public PullRequestDO commonPullRequestDataParser(JSONObject object) {
-		String title = (String) object.get("title");
-		String state = (String) object.get("state");
-		boolean closed = (boolean) object.get("closed");
-		String description = object.optString("description");
-		long updatedDate = (long) object.get("updatedDate");
-		long createdDate = (long) object.get("createdDate");
-		long closedDate = object.optLong("closedDate");
+    public PullRequestDO commonPullRequestDataParser(JSONObject object) {
+        String title = (String) object.get("title");
+        String state = (String) object.get("state");
+        boolean closed = (boolean) object.get("closed");
+        String description = object.optString("description");
+        long updatedDate = (long) object.get("updatedDate");
+        long createdDate = (long) object.get("createdDate");
+        long closedDate = object.optLong("closedDate");
 
         // Author Information
-		JSONObject author = object.getJSONObject("author");
-		JSONObject user = author.getJSONObject("user");
-		String emailAddress = user.optString("emailAddress");;
-		String displayName = (String) user.get("displayName");
-		String slug = (String) user.get("slug");
+        JSONObject author = object.getJSONObject("author");
+        JSONObject user = author.getJSONObject("user");
+        String emailAddress = user.optString("emailAddress");;
+        String displayName = (String) user.get("displayName");
+        String slug = (String) user.get("slug");
 
         // Reviewer Information
-		ArrayList<ReviewerDO> reviewerList = new ArrayList<ReviewerDO>();
-		JSONArray reviewers = object.getJSONArray("reviewers");
-		for(int i = 0 ; i <reviewers.length(); i++) {
+        ArrayList<ReviewerDO> reviewerList = new ArrayList<ReviewerDO>();
+        JSONArray reviewers = object.getJSONArray("reviewers");
+        for(int i = 0 ; i <reviewers.length(); i++) {
             JSONObject reviewer = reviewers.getJSONObject(i);
-			user = reviewer.getJSONObject("user");
+            user = reviewer.getJSONObject("user");
             String reviewerDisplayName = (String) user.get("displayName");
             String reviewerEmailAddress = user.optString("emailAddress");
             String status = reviewer.optString("status");
             boolean reviewerApproved = reviewer.optBoolean("approved");
             reviewerList.add(new ReviewerDO(reviewerDisplayName, reviewerEmailAddress, status, reviewerApproved));
-		}
-		return new PullRequestDO(title, state, closed, description, updatedDate, createdDate, closedDate,  emailAddress, displayName, slug, reviewerList);
-	}
+        }
+        return new PullRequestDO(title, state, closed, description, updatedDate, createdDate, closedDate,  emailAddress, displayName, slug, reviewerList);
+    }
 
     // TODO Branch url will be added onto constants, then we can call it while getting data
     public ArrayList<BranchDO> getBranchData(String url) throws UnirestException {
