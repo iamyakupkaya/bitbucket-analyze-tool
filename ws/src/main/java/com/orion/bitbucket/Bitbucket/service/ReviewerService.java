6806f877252ebc68c2ff9c7304e2f74cb21bd5bc
Oguzhan Ulusoy <oguzhan.ulusoy@mera.com>
2022-04-29 18:10:49 +0300

    resolved issue

--- ws/src/main/java/com/orion/bitbucket/Bitbucket/service/ReviewerService.java
+++ ws/src/main/java/com/orion/bitbucket/Bitbucket/service/ReviewerService.java
@@ -2,42 +2,42 @@ package com.orion.bitbucket.Bitbucket.service;
 
 import java.util.ArrayList;
 
-import com.orion.bitbucket.Bitbucket.model.AuthorDO;
+import com.orion.bitbucket.Bitbucket.model.ReviewerDO;
 
 public class ReviewerService extends BaseService implements ReviewerServiceIF{
 
 
-    public ArrayList<AuthorDO> getMergedPRReviewList() {
+    public ArrayList<ReviewerDO> getMergedPRReviewList() {
         // TODO Auto-generated method stub
         return null;
     }
 
 
-    public ArrayList<AuthorDO> getOpenPRReviewList() {
+    public ArrayList<ReviewerDO> getOpenPRReviewList() {
         // TODO Auto-generated method stub
         return null;
     }
 
 
-    public ArrayList<AuthorDO> getDeclinedPRReviewList() {
+    public ArrayList<ReviewerDO> getDeclinedPRReviewList() {
         // TODO Auto-generated method stub
         return null;
     }
 
 
-    public ArrayList<AuthorDO> getMergedPRReviewListByUsername(String username) {
+    public ArrayList<ReviewerDO> getMergedPRReviewListByUsername(String username) {
         // TODO Auto-generated method stub
         return null;
     }
 
 
-    public ArrayList<AuthorDO> getOpenPRReviewListByUsername(String username) {
+    public ArrayList<ReviewerDO> getOpenPRReviewListByUsername(String username) {
         // TODO Auto-generated method stub
         return null;
     }
 
 
-    public ArrayList<AuthorDO> getDeclinedPRReviewListByUsername(String username) {
+    public ArrayList<ReviewerDO> getDeclinedPRReviewListByUsername(String username) {
         // TODO Auto-generated method stub
         return null;
     }
@@ -61,7 +61,7 @@ public class ReviewerService extends BaseService implements ReviewerServiceIF{
     }
 
 
-    public ArrayList<AuthorDO> getReviewersByPRId(int id) {
+    public ArrayList<ReviewerDO> getReviewersByPRId(int id) {
         // TODO Auto-generated method stub
         return null;
     }
