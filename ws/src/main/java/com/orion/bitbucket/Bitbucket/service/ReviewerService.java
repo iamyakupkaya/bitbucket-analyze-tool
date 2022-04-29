9c79f5aafda559194e6e4571ed8d20164ed054d6
Oguzhan Ulusoy <oguzhan.ulusoy@mera.com>
2022-04-29 19:27:33 +0300

    implemented and renamed a method, and also added some design notes

--- ws/src/main/java/com/orion/bitbucket/Bitbucket/service/ReviewerService.java
+++ ws/src/main/java/com/orion/bitbucket/Bitbucket/service/ReviewerService.java
@@ -2,6 +2,7 @@ package com.orion.bitbucket.Bitbucket.service;
 
 import java.util.ArrayList;
 
+import com.orion.bitbucket.Bitbucket.model.PullRequestDO;
 import com.orion.bitbucket.Bitbucket.model.ReviewerDO;
 
 public class ReviewerService extends BaseService implements ReviewerServiceIF{
@@ -23,38 +24,53 @@ public class ReviewerService extends BaseService implements ReviewerServiceIF{
         // TODO Auto-generated method stub
         return null;
     }
+    
+    // En fazla review yapan kisi
+    // En fazla review edilen pull request
+    // En fazla review edilen 5 pull request (gereksiz olabilir bu)
+    // En az review yapan kisi
 
-
-    public ArrayList<ReviewerDO> getMergedPRReviewListByUsername(String username) {
-        // TODO Auto-generated method stub
-        return null;
+    // Returns a list that is consists of pull request reviewed by username
+    public ArrayList<PullRequestDO> getMergedPRListReviewedByUsername(String username) {
+        ArrayList<PullRequestDO> list = new ArrayList<PullRequestDO>();
+        ArrayList<PullRequestDO> mergedPRList = this.mergedPRList;
+        for (int i=0; i<mergedPRList.size(); i++) {
+            ArrayList<ReviewerDO> reviewers = mergedPRList.get(i).getReviewerList();
+            for (int j=0; j<reviewers.size(); j++) {
+                if (reviewers.get(j).getDisplayName().equals(username)) {
+                    list.add(mergedPRList.get(i));
+                }
+            }
+        }
+        return list;
     }
 
 
+    // TODO Rename and implement
     public ArrayList<ReviewerDO> getOpenPRReviewListByUsername(String username) {
         // TODO Auto-generated method stub
         return null;
     }
 
-
+    // TODO Rename and implement
     public ArrayList<ReviewerDO> getDeclinedPRReviewListByUsername(String username) {
         // TODO Auto-generated method stub
         return null;
     }
 
 
-    public int getMergedPRReviewCountByUsername(String username) {
-        // TODO Auto-generated method stub
-        return 0;
+    // Returns a number that indicates a specific user has reviewed pull requests that are merged
+    public int getMergedPRCountReviewedByUsername(String username) {
+        return this.getMergedPRListReviewedByUsername(username).size();
     }
 
-
+    // TODO Rename and implement
     public int getOpenPRReviewCountByUsername(String username) {
         // TODO Auto-generated method stub
         return 0;
     }
 
-
+    // TODO Rename and implement
     public int getDeclinedPRReviewCountByUsername(String username) {
         // TODO Auto-generated method stub
         return 0;
--- ws/src/main/java/com/orion/bitbucket/Bitbucket/service/ReviewerServiceIF.java
+++ ws/src/main/java/com/orion/bitbucket/Bitbucket/service/ReviewerServiceIF.java
@@ -1,5 +1,7 @@
 package com.orion.bitbucket.Bitbucket.service;
 import java.util.ArrayList;
+
+import com.orion.bitbucket.Bitbucket.model.PullRequestDO;
 import com.orion.bitbucket.Bitbucket.model.ReviewerDO;
 
 public interface ReviewerServiceIF {
@@ -10,11 +12,11 @@ public interface ReviewerServiceIF {
     public ArrayList<ReviewerDO> getDeclinedPRReviewList();
 
 
-    public ArrayList<ReviewerDO> getMergedPRReviewListByUsername(String username);
+    public ArrayList<PullRequestDO> getMergedPRListReviewedByUsername(String username);
     public ArrayList<ReviewerDO> getOpenPRReviewListByUsername(String username);
     public ArrayList<ReviewerDO> getDeclinedPRReviewListByUsername(String username);
 
-    public int getMergedPRReviewCountByUsername(String username);
+    public int getMergedPRCountReviewedByUsername(String username);
     public int getOpenPRReviewCountByUsername(String username);
     public int getDeclinedPRReviewCountByUsername(String username);
 
