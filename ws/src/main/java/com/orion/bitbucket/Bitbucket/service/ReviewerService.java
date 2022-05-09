ea4fb28cafd7929798b481481bf96dac67f672eb
yusufakbs <yusufakbas47@hotmail.com>
2022-05-09 13:54:38 +0300

    Added function for reviewer

--- ws/src/main/java/com/orion/bitbucket/Bitbucket/service/ReviewerService.java
+++ ws/src/main/java/com/orion/bitbucket/Bitbucket/service/ReviewerService.java
@@ -5,31 +5,76 @@ import java.util.ArrayList;
 import com.orion.bitbucket.Bitbucket.model.PullRequestDO;
 import com.orion.bitbucket.Bitbucket.model.ReviewerDO;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReviewerService extends BaseService implements ReviewerServiceIF {
 
 
+
     public ArrayList<ReviewerDO> getMergedPRReviewList() {
-        // TODO Auto-generated method stub
-        return null;
+        ArrayList<ReviewerDO> list = new ArrayList<ReviewerDO>();
+        ArrayList<PullRequestDO> mergePR = this.mergedPRList;
+        for (int i = 0; i < mergePR.size(); i++) {
+            ArrayList<ReviewerDO> reviewers = mergePR.get(i).getReviewerList();
+          for (int j = 0; j< reviewers.size(); j++) {
+            list.add(new ReviewerDO(reviewers.get(j).getDisplayName(), reviewers.get(j).getEmailAddress(), reviewers.get(j).getStatus(), reviewers.get(j).getApproved()));
+          }
+        }
+        System.out.println(list);
+        return list;
     }
 
 
     public ArrayList<ReviewerDO> getOpenPRReviewList() {
-        // TODO Auto-generated method stub
-        return null;
+        ArrayList<ReviewerDO> list = new ArrayList<ReviewerDO>();
+        ArrayList<PullRequestDO> openPR = this.openPRList;
+        for (int i = 0; i < openPR.size(); i++) {
+            ArrayList<ReviewerDO> reviewers = openPR.get(i).getReviewerList();
+          for (int j = 0; j< reviewers.size(); j++) {
+            list.add(new ReviewerDO(reviewers.get(j).getDisplayName(), reviewers.get(j).getEmailAddress(), reviewers.get(j).getStatus(), reviewers.get(j).getApproved()));
+          }
+        }
+        System.out.println(list);
+        return list;
     }
 
-
+    
     public ArrayList<ReviewerDO> getDeclinedPRReviewList() {
-        // TODO Auto-generated method stub
+        ArrayList<ReviewerDO> list = new ArrayList<ReviewerDO>();
+        ArrayList<PullRequestDO> declinedPR = this.declinedPRList;
+        for (int i = 0; i < declinedPR.size(); i++) {
+            ArrayList<ReviewerDO> reviewers = declinedPR.get(i).getReviewerList();
+          for (int j = 0; j< reviewers.size(); j++) {
+            list.add(new ReviewerDO(reviewers.get(j).getDisplayName(), reviewers.get(j).getEmailAddress(), reviewers.get(j).getStatus(), reviewers.get(j).getApproved()));
+          }
+        }
+        System.out.println(list);
+        return list;
+    }
+    // En fazla review yapan kisi
+    
+    public ArrayList<ReviewerDO> getAllReview() {
+        ArrayList<ReviewerDO> allReviewer = new ArrayList<ReviewerDO>();
+        allReviewer.addAll(getMergedPRReviewList());
+        allReviewer.addAll(getOpenPRReviewList());
+        allReviewer.addAll(getDeclinedPRReviewList());
+
+        return allReviewer;
+    }
+
+
+
+    public String getTopReviewer(){
+
+
         return null;
     }
 
-    // En fazla review yapan kisi
+  
     // En fazla review edilen pull request
     // En fazla review edilen 5 pull request (gereksiz olabilir bu)
     // En az review yapan kisi
-
     // Returns a list that is consists of pull request reviewed by username
     public ArrayList<PullRequestDO> getMergedPRListReviewedByUsername(String username) {
         ArrayList<PullRequestDO> list = new ArrayList<PullRequestDO>();
@@ -46,16 +91,34 @@ public class ReviewerService extends BaseService implements ReviewerServiceIF {
     }
 
 
-    // TODO Rename and implement
-    public ArrayList<ReviewerDO> getOpenPRReviewListByUsername(String username) {
-        // TODO Auto-generated method stub
-        return null;
+
+    public ArrayList<PullRequestDO> getOpenPRListReviewedByUsername(String username) {
+        ArrayList<PullRequestDO> list = new ArrayList<PullRequestDO>();
+        ArrayList<PullRequestDO> openPRList = this.openPRList;
+        for (int i = 0; i < openPRList.size(); i++){
+            ArrayList<ReviewerDO> reviewers = openPRList.get(i).getReviewerList();
+            for (int j = 0; j < reviewers.size(); j++){
+                if(reviewers.get(j).getDisplayName().equals(username)) {
+                   list.add(openPRList.get(i));
+               }
+            }
+        }
+        return list;
     }
 
-    // TODO Rename and implement
-    public ArrayList<ReviewerDO> getDeclinedPRReviewListByUsername(String username) {
-        // TODO Auto-generated method stub
-        return null;
+
+    public ArrayList<PullRequestDO> getDeclinedPRListReviewedByUsername(String username) {
+        ArrayList<PullRequestDO> list = new ArrayList<PullRequestDO>();
+        ArrayList<PullRequestDO> declinedPRList = this.declinedPRList;
+        for (int i = 0; i < declinedPRList.size(); i++){
+            ArrayList<ReviewerDO> reviewers = declinedPRList.get(i).getReviewerList();
+            for (int j = 0; j < reviewers.size(); j++){
+                if(reviewers.get(j).getDisplayName().equals(username)) {
+                   list.add(declinedPRList.get(i));
+               }
+            }
+        }
+        return list;
     }
 
 
@@ -64,23 +127,22 @@ public class ReviewerService extends BaseService implements ReviewerServiceIF {
         return this.getMergedPRListReviewedByUsername(username).size();
     }
 
-    // TODO Rename and implement
-    public int getOpenPRReviewCountByUsername(String username) {
-        // TODO Auto-generated method stub
-        return 0;
+    
+    public int getOpenPRCountReviewedByUsername(String username) {
+        return this.getOpenPRListReviewedByUsername(username).size();
     }
 
-    // TODO Rename and implement
-    public int getDeclinedPRReviewCountByUsername(String username) {
-        // TODO Auto-generated method stub
-        return 0;
+    
+    public int getDeclinedPRCountReviewedByUsername(String username) {
+        return this.getDeclinedPRListReviewedByUsername(username).size();
     }
 
 
+    
     public ArrayList<ReviewerDO> getReviewersByPRId(int id) {
         // TODO Auto-generated method stub
         return null;
     }
-
+    
 
 }
