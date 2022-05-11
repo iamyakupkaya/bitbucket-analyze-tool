1566668c0513e7d37dcee1ede07bf65f2b04d107
yusufakbs <yusufakbas47@hotmail.com>
2022-05-11 18:50:01 +0300

    added top author, reviewer and total

--- ws/src/main/java/com/orion/bitbucket/Bitbucket/controller/PageController.java
+++ ws/src/main/java/com/orion/bitbucket/Bitbucket/controller/PageController.java
@@ -1,6 +1,7 @@
 package com.orion.bitbucket.Bitbucket.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,7 @@ import com.orion.bitbucket.Bitbucket.model.AuthorDO;
 import com.orion.bitbucket.Bitbucket.service.AuthorServiceIF;
 import com.orion.bitbucket.Bitbucket.service.BaseServiceIF;
 import com.orion.bitbucket.Bitbucket.service.PullRequestServiceIF;
+import com.orion.bitbucket.Bitbucket.service.ReviewerServiceIF;
 
 @Controller
 public class PageController {
@@ -21,19 +23,45 @@ public class PageController {
     private BaseServiceIF baseService;
 
     @Autowired
-    private PullRequestServiceIF pullRequestService;
+    private PullRequestServiceIF pullRequestServiceIF;
 
     @Autowired
     private AuthorServiceIF authorServiceIF;
 
+    @Autowired
+    private ReviewerServiceIF reviewerServiceIF;
+    
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String edit(Model model) throws UnirestException {
         baseService.getData(); // Bu metot uygulamanin baslangicinda cagirilmali ki listeler doldurulsun. Hangi kisim ilk calisacaksa bu orada cagirilmali.
-        //List<AuthorDO> getAllAuthor = authorServiceIF.getCountOfPrStatesOfAllAuthor();
-        //List<PullRequest> pullrequests = service.reportAll();
-        //model.addAttribute("pr", new PullRequest());
-        //model.addAttribute("pullrequests", pullrequests);
+        model.addAttribute("authorCount", authorServiceIF.getAuthorCount());
+        model.addAttribute("pullRequestCount", pullRequestServiceIF.getAllPRCount());
+        model.addAttribute("reviewerCount", reviewerServiceIF.getAllReviewerCount());
+        model.addAttribute("reviewCount", reviewerServiceIF.getAllReviewCount());
+
+        String topAuthorDisplayName =null;
+        Long topAuthorPRsCount = null;
+        for (Map.Entry<String, Long> topAuthor : authorServiceIF.getTopAuthor().entrySet()) {
+            topAuthorDisplayName = topAuthor.getKey();
+            topAuthorPRsCount = topAuthor.getValue();
+            
+        }
+        model.addAttribute("topAuthorDisplayName", topAuthorDisplayName);
+        model.addAttribute("topAuthorPRsCount",topAuthorPRsCount); 
+        
+        String topReviewerDisplayName = null;
+        Long topReviewerCount = null;
+
+        for (Map.Entry<String, Long> topReviewer : reviewerServiceIF.getTopReviewer().entrySet()) {
+            topReviewerDisplayName = topReviewer.getKey();
+            topReviewerCount = topReviewer.getValue();
+            
+        }
+
+        model.addAttribute("topReviewerDisplayName", topReviewerDisplayName);
+        model.addAttribute("topReviewerCount",topReviewerCount); 
+
         return "index.html";
     }
 
@@ -44,13 +72,14 @@ public class PageController {
         model.addAttribute("authors", getAllAuthor);
         return "pull-requests.html";
     }
+    
+
 
     @RequestMapping(value = "/api/web-controller/test/@username={userName}")
     public String showAuthorDetails(Model model, @PathVariable(name = "userName", required = false) String userName) throws UnirestException {
         // List<UserPrDetails> UserPrDetails = service.filterPRsByName(userName);
         // model.addAttribute("prId", new UserPrDetails());
         // model.addAttribute("UserPrDetails", UserPrDetails);
-
 
         return "modalBox.html";
     }
