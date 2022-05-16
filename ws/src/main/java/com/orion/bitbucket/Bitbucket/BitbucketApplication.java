6d5617a525f98ba91ae3399cbc30ee281328ed33
yusufakbs <yusufakbas47@hotmail.com>
2022-05-16 17:37:08 +0300

    changes

--- ws/src/main/java/com/orion/bitbucket/Bitbucket/BitbucketApplication.java
+++ ws/src/main/java/com/orion/bitbucket/Bitbucket/BitbucketApplication.java
@@ -1,5 +1,9 @@
 package com.orion.bitbucket.Bitbucket;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +12,6 @@ public class BitbucketApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BitbucketApplication.class, args);
+
     }
 }
--- ws/src/main/java/com/orion/bitbucket/Bitbucket/service/AuthorService.java
+++ ws/src/main/java/com/orion/bitbucket/Bitbucket/service/AuthorService.java
@@ -1,16 +1,24 @@
 package com.orion.bitbucket.Bitbucket.service;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import com.orion.bitbucket.Bitbucket.model.AuthorDO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthorService extends BaseService implements AuthorServiceIF {
 
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
     @Autowired
     private PullRequestServiceIF pullRequestServiceIF;
 
@@ -23,11 +31,40 @@ public class AuthorService extends BaseService implements AuthorServiceIF {
         for (int i = 0; i < this.allPRList.size(); i++) {
             if (!authorList.contains(this.allPRList.get(i).getDisplayName())) {
                 authorList.add(this.allPRList.get(i).getDisplayName());
+                String sql = "INSERT INTO review (name)" +" VALUES ('"+this.allPRList.get(i).getDisplayName()+"')";
+                int rows = jdbcTemplate.update(sql);
+               
+                if(rows > 0) {
+                	System.out.println("Succes.");
+                }
             }
         }
+        
+        
+       
         return authorList;
     }
-
+//    String jdbcURL = "jdbc:postgresql://localhost:5432/bitbucket";
+//    String username = "postgres";
+//    String password = "12345";
+//    
+//    try {
+//    	Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+//    	System.out.println("Succes to db.");
+//    	String sql = "INSERT INTO Author (author_name, total_prs, total_merged"
+//    			+ ", total_open, total_declined)" +" VALUES ('"+this.allPRList.get(i).getDisplayName()+"'"+",4,1,1,1)";
+//    	Statement statement = connection.createStatement();
+//    	int rows = statement.executeUpdate(sql);
+//    	if(rows > 0) {
+//    		System.out.println("Başarılı");
+//    	}
+//    	connection.close();
+//    	
+//    	
+//    }catch (SQLException e) {
+//    	System.out.println("Başarısız.");
+//    	e.printStackTrace();
+//    }
     public ArrayList<AuthorDO> getCountOfPrStatesOfAllAuthor() {
         ArrayList<String> authorList = getAllAuthor();
         ArrayList<AuthorDO> authorDOList = new ArrayList<AuthorDO>();
