c39e1ace014bf50ea4a239e073d3f9355eeadc93
Oguzhan Ulusoy <oguzhan.ulusoy@mera.com>
2022-05-17 21:55:10 +0300

    developed db-related works

--- ws/src/main/java/com/orion/bitbucket/Bitbucket/service/PullRequestService.java
+++ ws/src/main/java/com/orion/bitbucket/Bitbucket/service/PullRequestService.java
@@ -1,80 +1,250 @@
 package com.orion.bitbucket.Bitbucket.service;
 
+import java.sql.*;
 import java.util.ArrayList;
 
+import com.orion.bitbucket.Bitbucket.dbc.TransactionManager;
 import com.orion.bitbucket.Bitbucket.model.PullRequestDO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PullRequestService extends BaseService implements PullRequestServiceIF {
 
-    public int getAllPRCount() {
-        return getMergedPRCount() + getOpenPRCount() + getDeclinedPRCount();
-    }
+    private final String SQL_GET_ALL_PR_COUNT = "select count(*) from pullrequest;";
+    private final String SQL_GET_MERGED_PR_COUNT = "select count(*) from pullrequest where state='MERGED';";
+    private final String SQL_GET_OPEN_PR_COUNT = "select count(*) from pullrequest where state='OPEN';";
+    private final String SQL_GET_DECLINED_PR_COUNT = "select count(*) from pullrequest where state='DECLINED';";
+    private final String SQL_GET_MERGED_PR_LIST = "select * from pullrequest where state='MERGED';";
+    private final String SQL_GET_OPEN_PR_LIST = "select * from pullrequest where state='OPEN';";
+    private final String SQL_GET_DECLINED_PR_LIST = "select * from pullrequest where state='DECLINED';";
+    private final String SQL_GET_MERGED_PR_LIST_BY_USERNAME = "select * from pullrequest where state='MERGED' and display_name=?;";
+    private final String SQL_GET_OPEN_PR_LIST_BY_USERNAME = "select * from pullrequest where state='OPEN' and display_name=?;";
+    private final String SQL_GET_DECLINED_PR_LIST_BY_USERNAME = "select * from pullrequest where state='DECLINED' and display_name=?;";
 
-    public int getMergedPRCount() {
-        return this.mergedPRList.size();
-    }
-
-    public int getOpenPRCount() {
-        return this.openPRList.size();
-    }
-
-    public int getDeclinedPRCount() {
-        return this.declinedPRList.size();
-    }
-
-    public ArrayList<PullRequestDO> getMergedPRList() {
-        return this.mergedPRList;
-    }
-
-    public ArrayList<PullRequestDO> getOpenPRList() {
-        return this.openPRList;
-    }
-
-    public ArrayList<PullRequestDO> getDeclinedPRList() {
-        return this.declinedPRList;
-    }
-
-    public ArrayList<PullRequestDO> getMergedPRListByUsername(String username) {
-        ArrayList<PullRequestDO> list = new ArrayList<PullRequestDO>();
-        for (int i = 0; i < this.mergedPRList.size(); i++) {
-            if (mergedPRList.get(i).getDisplayName().equals(username)) {
-                list.add(mergedPRList.get(i));
-            }
+    public int getAllPRCount() throws SQLException {
+        Connection connection = TransactionManager.getConnection();
+        int count = 0;
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(SQL_GET_ALL_PR_COUNT);
+        while (resultSet.next()) {
+            count = resultSet.getInt("count");
         }
+        resultSet.close();
+        statement.close();
+        connection.close();
+        return count;
+    }
+
+    public int getMergedPRCount() throws SQLException {
+        Connection connection = TransactionManager.getConnection();
+        int count = 0;
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(SQL_GET_MERGED_PR_COUNT);
+        while (resultSet.next()) {
+            count = resultSet.getInt("count");
+        }
+        resultSet.close();
+        statement.close();
+        connection.close();
+        return count;
+    }
+
+    public int getOpenPRCount() throws SQLException {
+        Connection connection = TransactionManager.getConnection();
+        int count = 0;
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(SQL_GET_OPEN_PR_COUNT);
+        while (resultSet.next()) {
+            count = resultSet.getInt("count");
+        }
+        resultSet.close();
+        statement.close();
+        connection.close();
+        return count;
+    }
+
+    public int getDeclinedPRCount() throws SQLException {
+        Connection connection = TransactionManager.getConnection();
+        int count = 0;
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(SQL_GET_DECLINED_PR_COUNT);
+        while (resultSet.next()) {
+            count = resultSet.getInt("count");
+        }
+        resultSet.close();
+        statement.close();
+        connection.close();
+        return count;
+    }
+
+    public ArrayList<PullRequestDO> getMergedPRList() throws SQLException {
+        ArrayList<PullRequestDO> mergedPRList = new ArrayList<PullRequestDO>();
+        Connection connection = TransactionManager.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(SQL_GET_MERGED_PR_LIST);
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String title = resultSet.getString("title");
+            String state = resultSet.getString("state");
+            boolean closed = resultSet.getBoolean("closed");
+            String description = resultSet.getString("description");
+            String updatedDate = resultSet.getString("update_date"); // long olsun. db ona gore duzeltilsin
+            String createdDate = resultSet.getString("created_date"); // long olsun. db ona gore duzeltilsin
+            String closedDate = resultSet.getString("closed_date"); // long olsun. db ona gore duzeltilsin
+            String emailAddress = resultSet.getString("email_address");
+            String displayName = resultSet.getString("display_name");
+            String slug = resultSet.getString("slug");
+            mergedPRList.add(new PullRequestDO(id, title, state, closed, description, updatedDate, createdDate, closedDate, emailAddress, displayName, slug, null));
+        }
+        resultSet.close();
+        statement.close();
+        connection.close();
+        return mergedPRList;
+    }
+
+    public ArrayList<PullRequestDO> getOpenPRList() throws SQLException {
+        ArrayList<PullRequestDO> openPRList = new ArrayList<PullRequestDO>();
+        Connection connection = TransactionManager.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(SQL_GET_OPEN_PR_LIST);
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String title = resultSet.getString("title");
+            String state = resultSet.getString("state");
+            boolean closed = resultSet.getBoolean("closed");
+            String description = resultSet.getString("description");
+            String updatedDate = resultSet.getString("update_date"); // long olsun. db ona gore duzeltilsin
+            String createdDate = resultSet.getString("created_date"); // long olsun. db ona gore duzeltilsin
+            String closedDate = resultSet.getString("closed_date"); // long olsun. db ona gore duzeltilsin
+            String emailAddress = resultSet.getString("email_address");
+            String displayName = resultSet.getString("display_name");
+            String slug = resultSet.getString("slug");
+            mergedPRList.add(new PullRequestDO(id, title, state, closed, description, updatedDate, createdDate, closedDate, emailAddress, displayName, slug, null));
+        }
+        resultSet.close();
+        statement.close();
+        connection.close();
+        return openPRList;
+    }
+
+    public ArrayList<PullRequestDO> getDeclinedPRList() throws SQLException {
+        ArrayList<PullRequestDO> declinedPRList = new ArrayList<PullRequestDO>();
+        Connection connection = TransactionManager.getConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(SQL_GET_DECLINED_PR_LIST);
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String title = resultSet.getString("title");
+            String state = resultSet.getString("state");
+            boolean closed = resultSet.getBoolean("closed");
+            String description = resultSet.getString("description");
+            String updatedDate = resultSet.getString("update_date"); // long olsun. db ona gore duzeltilsin
+            String createdDate = resultSet.getString("created_date"); // long olsun. db ona gore duzeltilsin
+            String closedDate = resultSet.getString("closed_date"); // long olsun. db ona gore duzeltilsin
+            String emailAddress = resultSet.getString("email_address");
+            String displayName = resultSet.getString("display_name");
+            String slug = resultSet.getString("slug");
+            mergedPRList.add(new PullRequestDO(id, title, state, closed, description, updatedDate, createdDate, closedDate, emailAddress, displayName, slug, null));
+        }
+        resultSet.close();
+        statement.close();
+        connection.close();
+        return declinedPRList;
+    }
+
+    public ArrayList<PullRequestDO> getMergedPRListByUsername(String username) throws SQLException {
+        ArrayList<PullRequestDO> list = new ArrayList<PullRequestDO>();
+        Connection connection = TransactionManager.getConnection();
+        PreparedStatement preparedStmt = null;
+        preparedStmt = connection.prepareStatement(SQL_GET_MERGED_PR_LIST_BY_USERNAME);
+        preparedStmt.setString(1, username);
+        ResultSet resultSet = preparedStmt.executeQuery();
+        connection.commit(); // ADDED
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String title = resultSet.getString("title");
+            String state = resultSet.getString("state");
+            boolean closed = resultSet.getBoolean("closed");
+            String description = resultSet.getString("description");
+            String updatedDate = resultSet.getString("update_date"); // long olsun. db ona gore duzeltilsin
+            String createdDate = resultSet.getString("created_date"); // long olsun. db ona gore duzeltilsin
+            String closedDate = resultSet.getString("closed_date"); // long olsun. db ona gore duzeltilsin
+            String emailAddress = resultSet.getString("email_address");
+            String displayName = resultSet.getString("display_name");
+            String slug = resultSet.getString("slug");
+            list.add(new PullRequestDO(id, title, state, closed, description, updatedDate, createdDate, closedDate, emailAddress, displayName, slug, null));
+        }
+        resultSet.close();
+        preparedStmt.close();
+        connection.close();
         return list;
     }
 
-    public ArrayList<PullRequestDO> getOpenPRListByUsername(String username) {
+    public ArrayList<PullRequestDO> getOpenPRListByUsername(String username) throws SQLException {
         ArrayList<PullRequestDO> list = new ArrayList<PullRequestDO>();
-        for (int i = 0; i < this.openPRList.size(); i++) {
-            if (openPRList.get(i).getDisplayName().equals(username)) {
-                list.add(openPRList.get(i));
-            }
+        Connection connection = TransactionManager.getConnection();
+        PreparedStatement preparedStmt = null;
+        preparedStmt = connection.prepareStatement(SQL_GET_OPEN_PR_LIST_BY_USERNAME);
+        preparedStmt.setString(1, username);
+        ResultSet resultSet = preparedStmt.executeQuery();
+        connection.commit(); // ADDED
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String title = resultSet.getString("title");
+            String state = resultSet.getString("state");
+            boolean closed = resultSet.getBoolean("closed");
+            String description = resultSet.getString("description");
+            String updatedDate = resultSet.getString("update_date"); // long olsun. db ona gore duzeltilsin
+            String createdDate = resultSet.getString("created_date"); // long olsun. db ona gore duzeltilsin
+            String closedDate = resultSet.getString("closed_date"); // long olsun. db ona gore duzeltilsin
+            String emailAddress = resultSet.getString("email_address");
+            String displayName = resultSet.getString("display_name");
+            String slug = resultSet.getString("slug");
+            list.add(new PullRequestDO(id, title, state, closed, description, updatedDate, createdDate, closedDate, emailAddress, displayName, slug, null));
         }
+        resultSet.close();
+        preparedStmt.close();
+        connection.close();
         return list;
     }
 
-    public ArrayList<PullRequestDO> getDeclinedPRListByUsername(String username) {
+    public ArrayList<PullRequestDO> getDeclinedPRListByUsername(String username) throws SQLException {
         ArrayList<PullRequestDO> list = new ArrayList<PullRequestDO>();
-        for (int i = 0; i < this.declinedPRList.size(); i++) {
-            if (declinedPRList.get(i).getDisplayName().equals(username)) {
-                list.add(declinedPRList.get(i));
-            }
+        Connection connection = TransactionManager.getConnection();
+        PreparedStatement preparedStmt = null;
+        preparedStmt = connection.prepareStatement(SQL_GET_DECLINED_PR_LIST_BY_USERNAME);
+        preparedStmt.setString(1, username);
+        ResultSet resultSet = preparedStmt.executeQuery();
+        connection.commit(); // ADDED
+        while (resultSet.next()) {
+            int id = resultSet.getInt("id");
+            String title = resultSet.getString("title");
+            String state = resultSet.getString("state");
+            boolean closed = resultSet.getBoolean("closed");
+            String description = resultSet.getString("description");
+            String updatedDate = resultSet.getString("update_date"); // long olsun. db ona gore duzeltilsin
+            String createdDate = resultSet.getString("created_date"); // long olsun. db ona gore duzeltilsin
+            String closedDate = resultSet.getString("closed_date"); // long olsun. db ona gore duzeltilsin
+            String emailAddress = resultSet.getString("email_address");
+            String displayName = resultSet.getString("display_name");
+            String slug = resultSet.getString("slug");
+            list.add(new PullRequestDO(id, title, state, closed, description, updatedDate, createdDate, closedDate, emailAddress, displayName, slug, null));
         }
+        resultSet.close();
+        preparedStmt.close();
+        connection.close();
         return list;
     }
 
-    public int getMergedPRCountByUsername(String username) {
+    public int getMergedPRCountByUsername(String username) throws SQLException {
         return getMergedPRListByUsername(username).size();
     }
 
-    public int getOpenPRCountByUsername(String username) {
+    public int getOpenPRCountByUsername(String username) throws SQLException {
         return getOpenPRListByUsername(username).size();
     }
 
-    public int getDeclinedPRCountByUsername(String username) {
+    public int getDeclinedPRCountByUsername(String username) throws SQLException {
         return getDeclinedPRListByUsername(username).size();
     }
 
--- ws/src/main/java/com/orion/bitbucket/Bitbucket/service/PullRequestServiceIF.java
+++ ws/src/main/java/com/orion/bitbucket/Bitbucket/service/PullRequestServiceIF.java
@@ -2,33 +2,34 @@ package com.orion.bitbucket.Bitbucket.service;
 
 import com.orion.bitbucket.Bitbucket.model.PullRequestDO;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public interface PullRequestServiceIF {
 
-    public int getAllPRCount();
+    public int getAllPRCount() throws SQLException;
 
-    public int getMergedPRCount();
+    public int getMergedPRCount() throws SQLException;
 
-    public int getOpenPRCount();
+    public int getOpenPRCount() throws SQLException;
 
-    public int getDeclinedPRCount();
+    public int getDeclinedPRCount() throws SQLException;
 
-    public ArrayList<PullRequestDO> getMergedPRList();
+    public ArrayList<PullRequestDO> getMergedPRList() throws SQLException;
 
-    public ArrayList<PullRequestDO> getOpenPRList();
+    public ArrayList<PullRequestDO> getOpenPRList() throws SQLException;
 
-    public ArrayList<PullRequestDO> getDeclinedPRList();
+    public ArrayList<PullRequestDO> getDeclinedPRList() throws SQLException;
 
-    public ArrayList<PullRequestDO> getMergedPRListByUsername(String username);
+    public ArrayList<PullRequestDO> getMergedPRListByUsername(String username) throws SQLException;
 
-    public ArrayList<PullRequestDO> getOpenPRListByUsername(String username);
+    public ArrayList<PullRequestDO> getOpenPRListByUsername(String username) throws SQLException;
 
-    public ArrayList<PullRequestDO> getDeclinedPRListByUsername(String username);
+    public ArrayList<PullRequestDO> getDeclinedPRListByUsername(String username) throws SQLException;
 
-    public int getMergedPRCountByUsername(String username);
+    public int getMergedPRCountByUsername(String username) throws SQLException;
 
-    public int getOpenPRCountByUsername(String username);
+    public int getOpenPRCountByUsername(String username) throws SQLException;
 
-    public int getDeclinedPRCountByUsername(String username);
+    public int getDeclinedPRCountByUsername(String username) throws SQLException;
 }
