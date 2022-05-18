f9e0a07a755a6051cac262d67d8fb91535b393f8
Oguzhan Ulusoy <oguzhan.ulusoy@mera.com>
2022-05-18 13:28:06 +0300

    commented out

--- ws/src/main/java/com/orion/bitbucket/Bitbucket/service/ReviewerService.java
+++ ws/src/main/java/com/orion/bitbucket/Bitbucket/service/ReviewerService.java
@@ -22,18 +22,18 @@ public class ReviewerService extends BaseService implements ReviewerServiceIF{
         int declinedReviewSize = 0;
         int approve = 0;
         int unApprove = 0;
-        for (int i = 0; i < reviewerList.size(); i++) {
-            mergeReviewSize = reviewServiceIF.getMergedPRCountReviewedByUsername(reviewerList.get(i));
-            openReviewSize = reviewServiceIF.getOpenPRCountReviewedByUsername(reviewerList.get(i));
-            declinedReviewSize = reviewServiceIF.getDeclinedPRCountReviewedByUsername(reviewerList.get(i));
-            total = mergeReviewSize + openReviewSize + declinedReviewSize;
-            System.out.println(reviewerList.get(i));
-            unApprove = reviewServiceIF.getReviewerUnApproveCount(reviewerList.get(i));
-            System.out.println(unApprove);
-            approve = reviewServiceIF.getReviewerApproveCount(reviewerList.get(i));
-            System.out.println(approve);
-            reviewerDOList.add(new ReviewerDO(0,reviewerList.get(i), total,approve,unApprove)); // id must be added
-        }
+        //for (int i = 0; i < reviewerList.size(); i++) {
+        //    mergeReviewSize = reviewServiceIF.getMergedPRCountReviewedByUsername(reviewerList.get(i));
+        //    openReviewSize = reviewServiceIF.getOpenPRCountReviewedByUsername(reviewerList.get(i));
+        //    declinedReviewSize = reviewServiceIF.getDeclinedPRCountReviewedByUsername(reviewerList.get(i));
+        //    total = mergeReviewSize + openReviewSize + declinedReviewSize;
+        //    System.out.println(reviewerList.get(i));
+        //    unApprove = reviewServiceIF.getReviewerUnApproveCount(reviewerList.get(i));
+        //    System.out.println(unApprove);
+        //    approve = reviewServiceIF.getReviewerApproveCount(reviewerList.get(i));
+        //    System.out.println(approve);
+        //    reviewerDOList.add(new ReviewerDO(0,reviewerList.get(i), total,approve,unApprove)); // id must be added
+        //}
         return reviewerDOList;
     }
 
@@ -44,14 +44,15 @@ public class ReviewerService extends BaseService implements ReviewerServiceIF{
     }
 
     public ArrayList<String> getAllReviewer() {
-        ArrayList<String> reviewerList = reviewServiceIF.getAllReviewer();
-        ArrayList<String> allReviewerList = new ArrayList<String>();
-        for (int i = 0; i < reviewerList.size(); i++) {
-            if (!allReviewerList.contains(reviewerList.get(i))) {
-                allReviewerList.add(reviewerList.get(i));
-            }
-        }
-        return reviewerList;
+        //ArrayList<String> reviewerList = reviewServiceIF.getAllReviewer();
+        //ArrayList<String> allReviewerList = new ArrayList<String>();
+        //for (int i = 0; i < reviewerList.size(); i++) {
+        //    if (!allReviewerList.contains(reviewerList.get(i))) {
+        //        allReviewerList.add(reviewerList.get(i));
+        //    }
+        //}
+        //return reviewerList;
+        return null;
     }
     
 }
